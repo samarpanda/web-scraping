44d9071d6e58ef166de5d4d76f9225f29904b3b7
Samar Panda <samar.panda84@gmail.com>
2015-06-11 09:50:43 +0530

    sample github explore links

--- noodle/index.js
+++ noodle/index.js
@@ -1,10 +1,9 @@
 var noodle = require('noodlejs');
 
 noodle.query({
-  url: 'http://samarpanda.com/',
-  type: 'html',
-  selector: 'a',
-  extract: 'href'
+  url:      'https://github.com/explore',
+  selector: '#explore-featured div ol li',
+  extract:  'href'
 })
 .then(function (results) {
   console.log(results);
